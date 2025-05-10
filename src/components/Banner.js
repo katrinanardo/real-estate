81a5c240fe155deca1348c5e3c41d9e44a115180
katrinanardo <99706445+katrinanardo@users.noreply.github.com>
2025-05-10 22:48:08 +0800

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -17,7 +17,7 @@ const Banner = () => {
       <div className='lg:ml-8 xl:ml-[30px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0 pt-[30px]'>
         <h1 className='text-4xl text-white lg:text-[64px] font-sans  leading-none mb-6 max-w-full lg:max-w-[40%]' style={{ fontFamily: "Georgia, serif" }}>Find Your Dream Home</h1>
         <p className='max-w-[480px] mb-8 text-white'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus accumsan dui sed orci fringilla aliquet ac in orci. Pellentesque gravida magna a luctus molestie. In aliquam odio eu arcu laoreet, in tempor purus pellentesque.
+         Discover your dream home with us — whether you're buying, selling, or investing, we provide expert guidance, trusted service, and a seamless real estate experience tailored just for you.
         </p>
         <button class="inline-block bg-white text-black px-4 py-2 rounded hover:bg-gray-200">
   Call 206-919-6886
